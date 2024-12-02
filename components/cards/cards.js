8b6f8839b74ab3d90c078ab3fe1b090cc2b21ee8
Maria Sola <lamarietaestudi@gmail.com>
2024-12-02 11:42:16 +0100

    corregido evento hover sobre las cards

--- components/cards/cards.js
+++ components/cards/cards.js
@@ -9,13 +9,7 @@ export function createCard(image) {
   const publicationInfo = document.createElement('div');
   const datePublication = document.createElement('p');
   const uploadIcon = document.createElement('img');
-  const visitButton = document.createElement('button');
-  const iconLikes = document.createElement('div');
-  const likesImage = document.createElement('img');
-  const likesText = document.createElement('p');
-  const iconCamera = document.createElement('div');
-  const cameraImage = document.createElement('img');
-  const cameraText = document.createElement('p');
+  const hoverContent = document.createElement('div');
 
   imageContainer.src = image.urls.regular;
   imageContainer.alt = image.alt_description;
@@ -26,38 +20,56 @@ export function createCard(image) {
   datePublication.textContent = new Date(image.created_at).toLocaleDateString();
   uploadIcon.src = './assets/icon-upload.svg';
   uploadIcon.alt = 'fecha de publicación';
-  visitButton.textContent = 'Visitar';
-  likesImage.src = './assets/icon-heart.svg';
-  likesImage.alt = 'likes';
-  likesText.textContent = `${image.likes}`;
-  cameraImage.src = './assets/icon-camera.svg';
-  cameraImage.alt = 'cámara';
-  cameraText.textContent = '+' + `${image.user.total_photos}`;
 
   card.className = 'card';
   imageContainer.className = 'imagecontent';
   profileContainer.className = 'profilecontainer';
   profileImage.className = 'profileimage';
   publicationInfo.className = 'publicationinfo';
-  visitButton.className = 'visitbutton';
-  iconLikes.className = 'card-icon likes';
-  iconCamera.className = 'card-icon camera';
+  hoverContent.className = 'hovercontent';
 
+  card.addEventListener('mouseenter', () => {
+    hoverContent.classList.add('visible');
+  });
+
+  card.addEventListener('mouseleave', () => {
+    hoverContent.classList.remove('visible');
+  });
+
+  card.addEventListener('touchstart', (event) => {
+    hoverContent.classList.toogle('visible');
+    event.stopPropagation();
+  });
+
+  const visitButton = document.createElement('button');
+  visitButton.textContent = 'Visitar';
+  visitButton.className = 'visitbutton';
   visitButton.addEventListener('click', () => {
     window.open(image.links.html, '_blank');
   });
 
-  publicationInfo.append(uploadIcon, datePublication);
+  const iconLikes = document.createElement('div');
+  iconLikes.className = 'card-icon likes';
+  const likesImage = document.createElement('img');
+  likesImage.src = './assets/icon-heart.svg';
+  likesImage.alt = 'likes';
+  const likesText = document.createElement('p');
+  likesText.textContent = `${image.likes}`;
   iconLikes.append(likesImage, likesText);
+
+  const iconCamera = document.createElement('div');
+  iconCamera.className = 'card-icon camera';
+  const cameraImage = document.createElement('img');
+  cameraImage.src = './assets/icon-camera.svg';
+  cameraImage.alt = 'cámara';
+  const cameraText = document.createElement('p');
+  cameraText.textContent = '+' + `${image.user.total_photos}`;
   iconCamera.append(cameraImage, cameraText);
+
+  hoverContent.append(iconLikes, iconCamera, visitButton);
+  publicationInfo.append(uploadIcon, datePublication);
   profileContainer.append(profileImage, profileName, publicationInfo);
-  card.append(
-    imageContainer,
-    profileContainer,
-    iconLikes,
-    iconCamera,
-    visitButton
-  );
+  card.append(imageContainer, hoverContent, profileContainer);
 
   return card;
 }
